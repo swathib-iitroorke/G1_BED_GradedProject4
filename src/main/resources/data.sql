c266be38bfb94bcb22571dce97e1f6ea9f6e7836
Swathi <Swathi@DESKTOP-NR8RUPT>
2024-02-26 14:45:55 +0530

    GradedProject4SB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into roles (role_id,name) values (1,'ADMIN');
+
+insert into users (user_id,password,username) values (1,'$2a$12$r5htAEPHde1FgovdMWFCw.1Wj6KIF2972A74d9Z3HKsqjc13BEGpK','ujjawal');
+
+insert into users_roles (user_id,role_id) values (1,1);
